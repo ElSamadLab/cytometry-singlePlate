2b8ed3e814958ff20ff47f793665468a219b51bf
Kieran Mace <kieran.mace@gmail.com>
2014-08-13 19:36:10 -0700

    Changed analyisis to be more generic

--- analysis.m
+++ analysis.m
@@ -1,36 +1,47 @@
+
+clearvars
+doses = 250./2.^(11:-1:0);
+[plate_p1,plate_order_p1] = fcs_plate_read('/home/kieran/Desktop/AA/140330_mixforlater_e2_p4_250_down_p1/96 Well - U bottom/')
+
+[plate_p2,plate_order_p2] = fcs_plate_read('/home/kieran/Desktop/AA/140330_mixforlater_e2_p4_250_down_p2/96 Well - U bottom/')
 %%
-% Old Media
-[plate,plate_order]= fcs_plate_read('/home/kieran/Desktop/oldMedia/96 Well - U bottom/')
+for i = 1:24, YFP_P1{i} = plate_p1(i).DATA(:,8) ./ plate_p1(i).DATA(:,5); end
+for i = 1:24, YFP_P1_CLEAN{i} = YFP_P1{i}(YFP_P1{i}>0); end
+
+for i = 1:24, YFP_P2{i} = plate_p2(i).DATA(:,8) ./ plate_p2(i).DATA(:,5); end
+for i = 1:24, YFP_P2_CLEAN{i} = YFP_P2{i}(YFP_P2{i}>0); end
+
 %%
-% New Media
-[plate,plate_order]= fcs_plate_read('/home/kieran/Desktop/newMedia/96 Well - U bottom/')
-%%
-for i = 1:96, YFP{i} = plate(i).DATA(:,8) ./ plate(i).DATA(:,5); end
-for i = 1:96, YFP_{i} = YFP{i}(YFP{i}>0), end
-%%
-y = YFP_
-%%
-figure
-ym = cellfun(@mean,y);
-ymr = reshape(ym,12,8);
-imagesc(log(ymr)')
-%%
-figure
-yv = cellfun(@var,y);
-yvr = reshape(yv,12,8)
-imagesc(log(yvr)')
-%%
-figure
-ycv2 = yv./(ym.^2);
-ycv2r = reshape(ycv2,12,8)
-imagesc(log(ycv2r)')
+% [a index] = sort(plate_order_p1);
+% YFP_P1 = YFP_P1(index);
+% YFP_P1_CLEAN = YFP_P1_CLEAN(index);
+% [a index] = sort(plate_order_p2);
+% YFP_P2 = YFP_P2(index);
+% YFP_P2_CLEAN = YFP_P2_CLEAN(index);
+
+y1 = YFP_P1_CLEAN;
+y2 = YFP_P2_CLEAN;
 %%
 figure
-for i = 1:96,
-subplot(8,12,i)
-[count, centers] = hist(log(y{i}),40);
-dist = count/sum(count);
-plot(centers, dist)
-xlim([-7 3]);
-ylim([0 0.4]);
-end
+ym1 = cellfun(@nanmedian,y1);
+ymr = reshape(ym1,12,2);
+subplot(211)
+semilogx(doses, ymr)
+title('linear')
+legend('Estradiol','Progesterone', 'location', 'northwest')
+subplot(212)
+semilogx(doses, log10(ymr))
+title('log')
+legend('Estradiol','Progesterone', 'location', 'northwest')
+%%
+figure
+ym1 = cellfun(@nanmedian,y2);
+ymr = reshape(ym1,12,2);
+subplot(211)
+semilogx(doses, ymr)
+title('linear')
+legend('Estradiol','Progesterone', 'location', 'northwest')
+subplot(212)
+semilogx(doses, log10(ymr))
+title('log')
+legend('Estradiol','Progesterone', 'location', 'northwest')
